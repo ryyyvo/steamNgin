de1de6be9c10b1b4035b6034fd7ca8dedb47b097
Ryan Vo <38389871+ryyyvo@users.noreply.github.com>
2024-06-26 23:01:37 -0700

    Attempt to get playerCount from appid 440 (Team Fortress 2)

--- packages/backend/databasedownloader.js
+++ packages/backend/databasedownloader.js
@@ -3,10 +3,35 @@ import fs from "fs";
 
 
 let client = new SteamUser();
+
+// async function main() {
+//     // Log in to Steam (use appropriate login details or methods)
+//     client.logOn();
+
+//     client.on('loggedOn', async () => {
+//         console.log('Logged into Steam');
+
+//         try {
+//             const appid = 440; // Replace with your desired AppID
+//             const result = client.getPlayerCount(appid);
+//             console.log(`Player count for appid ${appid}: ${result.playerCount}`);
+//         } catch (err) {
+//             console.error('Error getting player count:', err);
+//         } finally {
+//             client.logOff();
+//         }
+//     });
+// }
+
+// main().catch(console.error);
+
 client.logOn();
 
 client.on('loggedOn', async (details) => {
 	console.log('Logged onto Steam as ' + client.steamID.steam3());
+
+    let playerCount = client.getPlayerCount(440);
+    console.log("The player count is: " + playerCount);
 
 	console.log('Requesting appinfo for TF2 and CS:GO...');
 	let result = await client.getProductInfo([440, 730], [], true); // Passing true as the third argument automatically requests access tokens, which are required for some apps
