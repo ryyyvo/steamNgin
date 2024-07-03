5dbac0cedb98abaef31e2bae69124cdf01de82d9
Ryan Vo <38389871+ryyyvo@users.noreply.github.com>
2024-07-02 19:04:39 -0700

    Create getRemainingApps to get all games/software from https://api.steampowered.com/IStoreService/GetAppList/v1

--- packages/backend/app_list.js
+++ packages/backend/app_list.js
@@ -1,27 +1,70 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import fetch from "node-fetch";
-import fs from 'fs';
+import fs, { existsSync } from 'fs';
 
 const SECRET_KEY = process.env.STEAM_WEB_API_SECRET_KEY;
 
+const URL = `https://api.steampowered.com/IStoreService/GetAppList/v1/?key=${SECRET_KEY}&include_games=true&include_dlc=false&include_software=true&include_videos=false&include_hardware=false&max_results=50000`
+
 async function getAppList() {
-    const response = await fetch(`https://api.steampowered.com/IStoreService/GetAppList/v1/?key=${SECRET_KEY}&include_games=true&include_dlc=false&include_software=true&include_videos=false&include_hardware=false&max_results=50000`); 
-    // IStoreService
-
-    if (!response.ok) 
-        {
-            throw new Error('Could not retrieve applist ' + response.statusText);
+    try {
+        const response = await fetch(URL); 
+        // IStoreService
+    
+        if (!response.ok) 
+            {
+                throw new Error('Could not retrieve app list: ' + response.statusText);
+            }
+    
+        const data = await response.json();
+    
+        fs.writeFileSync('./packages/backend/app_list.json', JSON.stringify(data, null, 2));
+        console.log('Initial data saved.');
+    
+        if (data.response.have_more_results) {
+            await getRemainingApps(data.response.last_appid);
         }
-
-    const data = await response.json();
-
-    fs.writeFileSync('./packages/backend/app_list.json', JSON.stringify(data, null, 2));
-    console.log('Initial data saved.');
-
-    if (data.response.have_more_results) {
-        await getRemainingApps(data.responselast_appid)
     }
+    catch (error) {
+        console.error('Error getting app list:', error);
+    }
+
+}
+
+async function getRemainingApps(lastAppId) {
+    let hasMore = true;
+    let currentLastAppId = lastAppId;
+
+    while (hasMore) {
+        try {
+            // Modify URL to include lastAppId to get the next fetch call
+            const nextUrl = `${URL}&last_appid=${currentLastAppId}`;
+            const response = await fetch(nextUrl);
+            const data = await response.json();
+
+            // Append new apps to the existing JSON file 
+            const existingData = JSON.parse(fs.readFileSync('./packages/backend/app_list.json'));
+            existingData.response.apps.push(...data.response.apps);
+            fs.writeFileSync('./packages/backend/app_list.json', JSON.stringify(existingData, null, 2));
+            console.log('Appended new data');
+
+            // Update the lastAppId and hasMore variables
+            hasMore = data.response.have_more_results;
+            currentLastAppId = data.response.last_appid;
+        }
+        
+        catch (error) {
+            console.error('Error getting remaining app list:', error);
+            break;
+        }
+    }
+    
+    // Remove the have_more_results and last_appid JSON element from app_list 
+    const existingData = JSON.parse(fs.readFileSync('./packages/backend/app_list.json'));
+    delete existingData.response.have_more_results;
+    delete existingData.response.last_appid;
+    fs.writeFileSync('./packages/backend/app_list.json', JSON.stringify(existingData, null, 2));
 }
 
 async function getPlayerCount() {
