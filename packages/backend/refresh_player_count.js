fecd92ad17a1bae1203ed97cf199fdfeca639460
Ryan Vo <38389871+ryyyvo@users.noreply.github.com>
2024-07-20 22:06:09 -0700

    Adjust time to 12:00AM

--- packages/backend/refresh_player_count.js
+++ packages/backend/refresh_player_count.js
@@ -24,23 +24,23 @@ async function runTasks() {
 	// Function to run refreshTopGames every 10 minutes using recursive setTimeout
 	async function scheduleRefreshTopGames() {
 		if (!populatePlayerCountRunning && !refreshTopGamesRunning) {
-		refreshTopGamesRunning = true;
-		try {
-			console.log('Refreshing the top 5000 games');
-			await populatePlayerCount(1000); // Assuming 5000 is the number of top games
-		} catch (err) {
-			console.error(err);
-		} finally {
-			refreshTopGamesRunning = false;
-			console.log('refreshTopGames has finished');
-			setTimeout(scheduleRefreshTopGames, 10 * 60 * 1000); // Schedule next run in 10 minutes
-		}
+			refreshTopGamesRunning = true;
+			try {
+				console.log('Refreshing the top 5000 games');
+				await populatePlayerCount(100); // Assuming 5000 is the number of top games
+			} catch (err) {
+				console.error(err);
+			} finally {
+				refreshTopGamesRunning = false;
+				console.log('refreshTopGames has finished');
+				setTimeout(scheduleRefreshTopGames, 10 * 60 * 1000); // Schedule next run in 10 minutes
+			}
 		}
 	}
 	// Start running refreshTopGames immediately and every 10 minutes
 
 	// Schedule populatePlayerCount to run at 12 AM PST every day
-	schedule.scheduleJob('8 21 * * *', async () => {
+	schedule.scheduleJob('0 0 * * *', async () => {
 		if (refreshTopGamesRunning) {
 		console.log('Waiting for populatePlayerCount to complete before running the next task');
 		// Wait for populatePlayerCount to complete before starting the next task
