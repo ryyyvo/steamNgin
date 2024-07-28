5b99e594d5cb246ac9345cecabf080b6242a1e5d
Ryan Vo <38389871+ryyyvo@users.noreply.github.com>
2024-07-28 15:05:56 -0700

    Remove redundant console log message

--- packages/backend/src/db.js
+++ packages/backend/src/db.js
@@ -5,7 +5,6 @@ const MONGO_URI = process.env.MONGO_URI;
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI)
-    console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
     setTimeout(connectDB, 5000);
